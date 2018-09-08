6f08137fca2fbd7b734e11f2954da69235757420
Lucas Chen <shengch28@gmail.com>
2018-09-07 22:01:04 -0700

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 module.exports = {
     mLabURI: 'Your Own mLab credentials',
-    PLAID_CLIENT_ID : '5a6e3c548d92397b17411f21',
-    PLAID_SECRET : '7538ce069acd37db6b3f6adaaa45f9',
-    PLAID_PUBLIC_KEY : 'c33e0e4be0efac810ac297009186a8',
+    PLAID_CLIENT_ID : '',
+    PLAID_SECRET : '',
+    PLAID_PUBLIC_KEY : '',
 }
