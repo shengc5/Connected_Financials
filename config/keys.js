f16a7d1830ec367dbed2b4d22e7b291416b491c6
Lucas Chen <shengch28@gmail.com>
2018-09-07 21:41:34 -0700

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,5 @@
 module.exports = {
-	mongoURI: 'mongodb://localhost/users',
-    mLabURI: 'mongodb://shengch28:shengchen0628@ds121238.mlab.com:21238/mckinsey_intern',
+    mLabURI: 'Your Own mLab credentials',
     PLAID_CLIENT_ID : '5a6e3c548d92397b17411f21',
     PLAID_SECRET : '7538ce069acd37db6b3f6adaaa45f9',
     PLAID_PUBLIC_KEY : 'c33e0e4be0efac810ac297009186a8',
